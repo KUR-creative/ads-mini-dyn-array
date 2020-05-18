6e6864d9430937577b4b5ffd6b80527e6cc231dd
kur <rhdnfka94@gmail.com>
2020-05-18 13:55:12 +0900

    Add merge

--- dyn_test.py
+++ dyn_test.py
@@ -1,36 +1,53 @@
 import funcy as F
+import pytest
 from hypothesis import given
 from hypothesis import strategies as st
 
 from main import *
 
 @given(st.lists(st.integers()).map(sorted))
-def test_ctor(lst):
-    print(lst)
+def test_ctor(xs):
+    print(xs)
     
 @given(st.iterables(st.integers()))
-def test_length_of_arrays_of_dyn_arr_are_all_pow_of_2(lst):
+def test_length_of_arrays_of_dyn_arr_are_all_pow_of_2(xs):
     dyn_arr = dynamic_array()
-    for x in lst:
+    for x in xs:
         insert(dyn_arr, x)
         for arr in dyn_arr:
             assert is_power_of_two(len(arr))
             
 @given(st.iterables(st.integers()))
-def test_length_of_arrays_of_dyn_arr_are_all_pow_of_2(lst):
+def test_length_of_arrays_of_dyn_arr_are_all_pow_of_2(xs):
     dyn_arr = dynamic_array()
-    for x in lst:
+    for x in xs:
         insert(dyn_arr, x)
         for arr in dyn_arr:
             assert is_power_of_two(len(arr))
 
 @given(st.iterables(st.integers()))
-def test_length_of_arrays_of_dyn_arr_are_all_different(lst):
+def test_length_of_arrays_of_dyn_arr_are_all_different(xs):
     dyn_arr = dynamic_array()
-    for x in lst:
+    for x in xs:
         insert(dyn_arr, x)
         
         assert len(dyn_arr) == len(F.ldistinct(dyn_arr, key=len))
 
+@pytest.mark.skip(reason="no way of currently testing this")
+@given(st.lists(st.integers()))
+def test_added_elements_are_all_saved_to_dyn_arr(xs):
+    dyn_arr = dynamic_array()
+    for x in xs:
+        insert(dyn_arr, x)
 
-#def test_arrays_of_dyn_arr_are_all_sorted_itself(lst):
+    assert len(F.lflatten(dyn_arr)) == len(xs)
+    assert set(F.flatten(dyn_arr)) == set(xs)
+#def test_arrays_of_dyn_arr_are_all_sorted_itself(xs):
+
+@given(st.lists(st.integers()).map(sorted),
+       st.lists(st.integers()).map(sorted))
+def test_merge(xs, ys):
+    res = merge(xs, ys)
+    assert len(res) == len(xs) + len(ys)
+    # check sorted
+    assert all(res[i] <= res[i+1] for i in range(len(res) - 1))
--- main.py
+++ main.py
@@ -4,5 +4,35 @@ def is_power_of_two(n):
 def dynamic_array():
     return []
 
+def merge(sorted1, sorted2):
+    i1,i2,idx = 0,0,0
+    ret = [None] * (len(sorted1) + len(sorted2))
+    while i1 + i2 < len(sorted1) + len(sorted2):
+        if i1 == len(sorted1):
+            ret[idx] = sorted2[i2]; i2 += 1
+        elif i2 == len(sorted2):
+            ret[idx] = sorted1[i1]; i1 += 1
+        elif sorted1[i1] <= sorted2[i2]: # Basic Operation
+            ret[idx] = sorted1[i1]; i1 += 1
+        else:
+            ret[idx] = sorted2[i2]; i2 += 1
+        idx += 1
+    return ret
+    
 def insert(dyn_arr, elem):
-    dyn_arr.append((elem,))
+    '''
+    prev = []
+    for no,arr in enumerate(dyn_arr):
+        if len(arr) == 2**no:
+            prev = dyn_arr[no]
+            del dyn_arr[no]
+        else:
+            dyn_arr
+            prev = []
+    if prev:
+        dyn_arr.append(prev)
+    '''
+    if dyn_arr:
+        pass
+    else:
+        dyn_arr.append((elem,))
